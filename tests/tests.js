f9f09d6af6f1ccc8691da168ae88001c3d29ef60
Oliver Sturm <oliver@oliversturm.com>
2017-08-28 16:24:38 +0100

    tests added

--- tests/tests.js
+++ tests/tests.js
@@ -1,11 +1,67 @@
 const path = require('path');
 const yt = require('yeoman-test');
+const assert = require('yeoman-assert');
 
-describe('devextreme:app', function() {
-  it('accepts apptype as an argument', function() {
-    yt
-      .run(path.join(__dirname, '../generators/app'))
-      .withArguments(['knockout'])
-      .then(function() {});
+describe('devextreme:app js', function() {
+  describe('webpack', function() {
+    describe('jquery', function() {
+      it('default params', function(done) {
+        yt
+          .run(path.join(__dirname, '../generators/app'))
+          .withArguments(['jquery'])
+          .then(() => {
+            assert.file([
+              'src/index.html',
+              'src/index.js',
+              'README.md',
+              'webpack.common.js',
+              'webpack.dev.js',
+              'webpack.prod.js',
+              'package.json',
+              '.gitignore'
+            ]);
+            assert.fileContent('src/index.js', /require..globalize/);
+            assert.fileContent('webpack.common.js', /globalize: path.resolve/);
+            assert.fileContent('package.json', 'cldr-data');
+            assert.fileContent('package.json', 'globalize');
+            done();
+          });
+      });
+
+      it('addlang de', function(done) {
+        yt
+          .run(path.join(__dirname, '../generators/app'))
+          .withArguments(['jquery'])
+          .withOptions({
+            addlang: 'de'
+          })
+          .then(() => {
+            assert.file([
+              'src/index.html',
+              'src/index.js',
+              'README.md',
+              'webpack.common.js',
+              'webpack.dev.js',
+              'webpack.prod.js',
+              'package.json',
+              '.gitignore'
+            ]);
+            assert.fileContent('src/index.js', /require..globalize/);
+            assert.fileContent(
+              'src/index.js',
+              'cldr-data/main/de/ca-gregorian.json'
+            );
+            assert.fileContent(
+              'src/index.js',
+              'devextreme/localization/messages/de.json'
+            );
+            assert.fileContent('src/index.js', 'locale(navigator');
+            assert.fileContent('webpack.common.js', /globalize: path.resolve/);
+            assert.fileContent('package.json', 'cldr-data');
+            assert.fileContent('package.json', 'globalize');
+            done();
+          });
+      });
+    });
   });
 });
